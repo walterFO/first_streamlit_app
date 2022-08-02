e9e317209aa16b42ae637315af158c0b863a5588
walterFO <64021406+walterFO@users.noreply.github.com>
2022-08-02 18:40:40 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,6 @@
 import streamlit
 streamlit.title('My Parents New Healthy Diner')
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry')
+streamlit.text('Cale 3 & Spinash')
+streamlit.text('Hard-Boiled Free-Range Egg')
