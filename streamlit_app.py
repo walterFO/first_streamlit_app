c100b0cd4078ba02bf7078081ac9b717a0f35ba9
walterFO <64021406+walterFO@users.noreply.github.com>
2022-08-04 17:04:43 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,7 @@
 import streamlit
+import snowflake.connector
+import pandas
+import requests
 
 streamlit.title('My Parents New Healthy Diner')
 
@@ -10,7 +13,7 @@ streamlit.text('🥑🍞 Avocado Toast')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
-import pandas
+#import pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
 
@@ -24,7 +27,7 @@ streamlit.dataframe(fruits_to_show)
 # Display fruityvice API response
 streamlit.header("Fruityvice Fruit Advice!")
 
-import requests
+#import requests
 fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
 streamlit.write('The user entered ', fruit_choice)
 
@@ -39,7 +42,7 @@ streamlit.dataframe(fruityvice_normalized)
 # Don't run anything past here while we troubleshoot
 streamlit.stop
 
-import snowflake.connector
+#import snowflake.connector
 
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
